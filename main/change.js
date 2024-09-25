de3d68fc2ef7d5f347fada9982dc19128e1ee1a1
ツジ <ituzi27@gmail.com>
2024-09-26 02:40:08 +0900

    Add files via upload

---
+++ main/change.js
@@ -0,0 +1,14 @@
+function ego1(){
+    let imag = document.getElementById("ego");
+    imag.innerHTML = '<img src="ego/asela1.png" alt="">';
+}
+
+function ego2(){
+    let imag = document.getElementById("ego");
+    imag.innerHTML = '<img src="ego/asela2.png" alt="">';
+}
+
+function ego3(){
+    let imag = document.getElementById("ego");
+    imag.innerHTML = '<img src="ego/asela3.png" alt="">';
+}
