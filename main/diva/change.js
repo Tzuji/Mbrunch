bc140a1ffb9c1052b36ac16eeed611c5acfdf924
ツジ <ituzi27@gmail.com>
2024-09-26 15:43:38 +0900

    Add files via upload

---
+++ main/diva/change.js
@@ -0,0 +1,14 @@
+function ego1(){
+    let imag = document.getElementById("ego");
+    imag.innerHTML = '<img src="ego/diva1.png" alt="">';
+}
+
+function ego2(){
+    let imag = document.getElementById("ego");
+    imag.innerHTML = '<img src="ego/diva2.png" alt="">';
+}
+
+function ego3(){
+    let imag = document.getElementById("ego");
+    imag.innerHTML = '<img src="ego/diva3.png" alt="">';
+}
---
+++ main/miho/panic.js
@@ -0,0 +1,4 @@
+function exe(){
+    let imag = document.getElementById("panic");
+    imag.innerHTML = '<img src="miho/panic.png" alt="panic">';
+}
