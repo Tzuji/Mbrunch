3bc553338535d832e9811aaddc028c2a5dbe071a
ツジ <ituzi27@gmail.com>
2024-09-27 20:57:09 +0900

    Add files via upload

---
+++ main/asela/change.js
@@ -0,0 +1,47 @@
+let gift = 0 , ego = 1;
+
+function condition(){ 
+    if(gift == 0){
+        gift = 1;
+    }else{
+        gift = 0;
+    }
+
+    if(ego == 1){
+        ego1();
+    }else if(ego ==2){
+        ego2();
+    }else{
+        ego3();
+    }
+}
+
+function ego1(){
+    let imag = document.getElementById("ego");
+    ego = 1;
+    if(gift == 0){
+        imag.style.backgroundImage = "url(ego/gift.png),url(ego/asela1.png);";      
+    }else{
+        imag.style.backgroundImage = "url(ego/asela1.png)";
+    }
+}
+
+function ego2(){
+    let imag = document.getElementById("ego");
+    ego = 2;
+    if(gift == 0){
+        imag.style.backgroundImage = "url(ego/gift.png),url(ego/asela2.png);";
+    }else{
+        imag.style.backgroundImage = "url(ego/asela2.png)";
+    }
+}
+
+function ego3(){
+    let imag = document.getElementById("ego");
+    ego = 3;
+    if(gift == 0){
+        imag.style.backgroundImage = "url(ego/gift.png), url(ego/asela3.png);";
+    }else{
+        imag.style.backgroundImage = "url(ego/asela3.png)";
+    }
+}
