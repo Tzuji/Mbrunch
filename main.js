6d9fabc7601721d09820345b8f9d37524c548bac
ツジ <ituzi27@gmail.com>
2024-11-19 02:31:38 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 //マウスストーカー用のdivを取得
 const stalker = document.getElementById('cursor'); 
-
+let x, y, ox = 0;
 //上記のdivタグをマウスに追従させる処理
 document.addEventListener('mousemove', function (e) {
-    stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
+    x = e.clientX;
+    y = e.clientY;
+    if(x - ox > 0 ){
+        stalker.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+    }else{
+        stalker.style.transform = 'translate(' + x + 'px, ' + y + 'px) scale(-1,1)';
+    }
+    ox = x;
 });
+
